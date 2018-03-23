38038695ddf8ef485163bc5434342115b62eaaaa
Guangmiao Luan <luangm@gmail.com>
2018-03-23 11:11:44 +0800

    es6

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: './src/index.ts',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
+        filename: 'index.es6.js',
         publicPath: '/dist/',
         libraryTarget: 'umd'
     },
