c3a25b4e5ad06ba87f24b94d480c4cb26e98fa5c
Guangmiao Luan <luangm@gmail.com>
2018-03-23 11:20:06 +0800

    es6 repack

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: './src/index.ts',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index.es6.js',
+        filename: 'index.js',
         publicPath: '/dist/',
         libraryTarget: 'umd'
     },
