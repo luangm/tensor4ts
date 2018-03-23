5bb16ed3d2d6fc06d9f02d802304b5450409bc71
Guangmiao Luan <luangm@gmail.com>
2018-03-23 23:34:12 +0800

    shape equals

--- src/utils/ShapeUtils.ts
+++ src/utils/ShapeUtils.ts
@@ -184,4 +184,16 @@ export default class ShapeUtils {
     }
     return result;
   }
+
+  static shapeEquals(a: number[], b: number[]): boolean {
+    if (a == null || b == null || a.length != b.length) {
+      return false;
+    }
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] != b[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
